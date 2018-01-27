76cea24f27a8478bd3afc2a17b1bc590a8bb76c2
liangbin.hu <liangbin.hu@hotmail.com>
2018-01-27 19:47:12 +0800

    搜索

--- app/src/views/index.py
+++ app/src/views/index.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-from flask import render_template, request
+from flask import render_template
 
 from app import app
 from app.src.models.post import Post
@@ -8,8 +8,9 @@ from app.src.models.post import Post
 @app.route('/')
 @app.route('/index')
 @app.route('/index/<int:page>')
-def index(page=1):
-    interval = 3
+@app.route('/index/<string:st>')
+def index(page=1, st=''):
+    interval = 3 if st == '' else 100
     count = Post.query.count()
     max_page = count // interval + (1 if count % interval > 0 else 0)
     max_page = 1 if max_page < 1 else max_page
@@ -18,16 +19,9 @@ def index(page=1):
     page = min(max_page, page)
 
     # 查询最近的文章
-    posts = Post.query.filter(Post.temp == 1).order_by(Post.id.desc()).offset((page - 1) * interval).limit(interval)
+    posts = Post.query.filter(
+        Post.title.like('%' + st + '%') | Post.tags.like('%' + st + '%')).filter(Post.temp == 1).order_by(
+        Post.id.desc()).offset((page - 1) * interval).limit(interval)
 
     return render_template("index.html", title=u"主页", recent_posts=posts, querypage=page, max_page=max_page,
-                           child_title=u"最新发布")
-
-
-@app.route("/search", methods=['POST'])
-def search():
-    search_text = request.form['st']
-    posts = Post.query.filter(
-        Post.title.like('%' + search_text + '%') | Post.tags.like('%' + search_text + '%')).order_by(Post.id.desc())
-    print posts
-    return render_template("index.html", title=u"搜索结果", recent_posts=posts, querypage=1, max_page=1, child_title=u"搜索结果")
+                           child_title=u"最新发布" if st == '' else u"搜索结果")
