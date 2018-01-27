4ce7da20ad7d892e939c1a167d79705b387bab0a
liangbin.hu <liangbin.hu@hotmail.com>
2018-01-27 18:01:14 +0800

    增加搜索功能

--- app/src/views/index.py
+++ app/src/views/index.py
@@ -8,8 +8,9 @@ from app.src.models.post import Post
 @app.route('/')
 @app.route('/index')
 @app.route('/index/<int:page>')
-def index(page=1):
-    interval = 3
+@app.route('/index/<string:st>')
+def index(page=1, st=''):
+    interval = 3 if st == '' else 100
     count = Post.query.count()
     max_page = count // interval + (1 if count % interval > 0 else 0)
     max_page = 1 if max_page < 1 else max_page
@@ -18,6 +19,7 @@ def index(page=1):
     page = min(max_page, page)
 
     # 查询最近的文章
-    posts = Post.query.filter_by(temp=1).order_by(Post.id.desc()).offset((page - 1) * interval).limit(interval)
+    posts = Post.query.filter_by(temp=1).filter_by(title.like('%' + st + '%') | tags.like('%' + st + '%')).order_by(
+        Post.id.desc()).offset((page - 1) * interval).limit(interval)
 
     return render_template("index.html", title=u"主页", recent_posts=posts, querypage=page, max_page=max_page)
